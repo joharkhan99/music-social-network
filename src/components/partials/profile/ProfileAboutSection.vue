75bdb0f6dfb0414f492f5f68eb9754460016a187
single-byte <70197335+single-byte@users.noreply.github.com>
2023-08-10 20:11:26 +0500

    added ProfileSection

---
+++ src/components/partials/profile/ProfileAboutSection.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="py-2">
+    <p class="text-gray-900 text-lg">
+      <b>About me</b>
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda magnam esse officiis quas autem. Voluptas quas itaque ipsam consectetur quisquam eos, sit consequatur praesentium quaerat velit!
+      Voluptas autem cumque consequatur!
+    </p>
+  </div>
+</template>
---
+++ src/views/account/ProfileSection.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container max-w-4xl mx-auto flex">
+    <div class="w-1/3">
+      <img src="https://via.placeholder.com/500" alt="user img" class="w-full rounded-lg h-auto shadow-lg">
+    </div>
+
+    <div class="w-full pl-4">
+      <div class="flex">
+        <div class="w-1/2">
+          <h1 class="text-2xl md:text-4xl text-left text-gray-900">John Doe</h1>
+          <span class="text-md text-gray-700">
+            <i><b>London, UK</b></i>
+          </span>
+        </div>
+        <div class="w-1/2 mt-2">
+          <RouterLinkButton btn-text="Edit Profile" color="green" url="/account/edit-profile" />
+        </div>
+      </div>
+
+      <ProfileInfoSection />
+      <ProfileAboutSection />
+
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import ProfileInfoSection from '@/components/partials/profile/ProfileInfoSection.vue'
+import ProfileAboutSection from '@/components/partials/profile/ProfileAboutSection.vue'
+import RouterLinkButton from '@/components/global/RouterLinkButton.vue';
+</script>
